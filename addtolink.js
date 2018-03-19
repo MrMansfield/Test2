9e6280134ba17854a9a15aac926789b430664b0f
MrMansfield <36575001+MrMansfield@users.noreply.github.com>
2018-03-18 21:21:08 -0500

    Create addtolink.js

---
+++ addtolink.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $("#button").click(function() {
+        window.location = "https://en.wikipedia.org/wiki/" + $("#numberInput").val();
+    });                          
+
+});
